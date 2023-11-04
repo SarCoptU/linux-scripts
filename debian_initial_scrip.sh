2fa0b9cfa298a1c4dc71155fe43f2461b0bd658e
SarCoptU <119886030+SarCoptU@users.noreply.github.com>
2023-11-04 11:45:35 +0000

    Update debian_initial_scrip.sh

--- debian_initial_scrip.sh
+++ debian_initial_scrip.sh
@@ -27,3 +27,8 @@ sudo nala install discord.deb
 
 # install taskbar notification ?site
 # fingerprint authentication https://wiki.debian.org/SecurityManagement/fingerprint%20authentication
+
+# install obsidian
+# wget 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/obsidian_1.4.16_amd64.deb'
+# chmod +x obsidian*.deb
+# gdebi obsidian*.deb
