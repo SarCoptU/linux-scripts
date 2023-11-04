99e235856d851f0f11be13a88d20b4700f714542
SarCoptU <119886030+SarCoptU@users.noreply.github.com>
2023-11-04 17:17:35 +0000

    Update debian_initial.sh

--- debian_initial.sh
+++ debian_initial.sh
@@ -30,6 +30,6 @@ sudo nala install discord.deb
 # fingerprint authentication https://wiki.debian.org/SecurityManagement/fingerprint%20authentication
 
 # install obsidian
-# wget 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/obsidian_1.4.16_amd64.deb'
-# chmod +x obsidian*.deb
-# gdebi obsidian*.deb
+wget 'https://github.com/obsidianmd/obsidian-releases/releases/download/v*.deb'
+chmod +x obsidian*.deb
+gdebi obsidian*.deb
