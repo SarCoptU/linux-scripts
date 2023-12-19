0fd469df8d0821b90e673872ec6a2c4dba3ac89f
Remus Sferle <119886030+SarCoptU@users.noreply.github.com>
2023-12-19 22:17:10 +0000

    Update deb_12_sec.sh

--- Debian/deb_12_sec.sh
+++ Debian/deb_12_sec.sh
@@ -6,12 +6,12 @@ sudo nala update && sudo apt upgrade -y
 # install basic security tools
 sudo nala install -y gobuster openvpn nmap whatweb gdb gnupg2 metasploit-framework exploitdb 
 
-# install seclists
-git clone https://github.com/danielmiessler/SecLists.git
-# move seclists to /etc/share
-
 # install gdb and pwngdb plugin
 sudo nala install -y gdb
 git clone https://github.com/pwndbg/pwndbg
 cd pwndbg
 ./setup.sh
+
+# install seclists
+git clone https://github.com/danielmiessler/SecLists.git
+# move seclists to /etc/share
