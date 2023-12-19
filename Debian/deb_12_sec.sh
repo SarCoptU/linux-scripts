ec7889fbe9028e75ca27dcc407dd276c46423b72
Remus Sferle <119886030+SarCoptU@users.noreply.github.com>
2023-12-19 20:20:14 +0000

    Update deb_12_sec.sh

--- Debian/deb_12_sec.sh
+++ Debian/deb_12_sec.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
-sudo apt install gobuster openvpn nmap whatweb gdb gnupg2
-
-# install metasploit - one way is to enable kali repos, install needed apps and disable
-  # never apt upgrade while kali repos active
-
-# learn a bit about gnupg https://www.redhat.com/sysadmin/getting-started-gpg
-
-# # seclists : https://github.com/danielmiessler/SecLists
-# need to add a firewall rule to allow all traffic to tun0 in and out
-# # issues for wordlists as they exist in kali - can't find reputable github page
-
-# searchsploit https://www.exploit-db.com/searchsploit
-
-# pwndbg - install by github https://github.com/pwndbg/pwndbg
-
-# ghidra = run from ghidra run file once JDK latest is installed https://github.com/NationalSecurityAgency/ghidra
+sudo nala update && sudo apt upgrade -y
+sudo nala install -y gobuster openvpn nmap whatweb gdb gnupg2 metasploit-framework exploitdb 
