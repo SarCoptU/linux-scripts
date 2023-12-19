125419da856202ee6187dbe6659c62ee12348e2b
Remus Sferle <119886030+SarCoptU@users.noreply.github.com>
2023-12-19 20:23:02 +0000

    Update deb_12_sec.sh

--- Debian/deb_12_sec.sh
+++ Debian/deb_12_sec.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+# update the system and repos
 sudo nala update && sudo apt upgrade -y
+
+# install basic security tools
 sudo nala install -y gobuster openvpn nmap whatweb gdb gnupg2 metasploit-framework exploitdb 
+
+# install gdb and pwngdb plugin
+sudo nala install -y gdb
+
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+./setup.sh
