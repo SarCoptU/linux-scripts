1f83fab209f32a10b82aa1abcfbd5f22a9a98a36
Remus Sferle <119886030+SarCoptU@users.noreply.github.com>
2023-12-27 18:27:43 +0000

    Update deb_12_initial.sh

--- Debian/deb_12_initial.sh
+++ Debian/deb_12_initial.sh
@@ -56,6 +56,17 @@ sudo nala install -y fonts-powerline
 # restart terminal and apply changes
 source ~/.zshrc
 
+# VS code install from https://code.visualstudio.com/docs/setup/linux
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
+
 # install obsidian - instruction on https://obsidian.md/download
 
 # install VMware from https://www.vmware.com/products/workstation-player/workstation-player-evaluation.html
