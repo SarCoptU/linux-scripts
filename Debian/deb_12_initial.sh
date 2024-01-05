9a7c0c7fd567edce14ef5936fbf4dd105e910b47
Remus Sferle <119886030+SarCoptU@users.noreply.github.com>
2024-01-05 16:41:54 +0000

    Update deb_12_initial.sh

--- Debian/deb_12_initial.sh
+++ Debian/deb_12_initial.sh
@@ -7,7 +7,7 @@ rm -rf ~/Videos ~/Templates ~/Music ~/Public
 sudo apt update
 sudo apt upgrade
 
-sudo apt install tmux neovim htop wget curl timeshift gdebi neofetch nala git flatpak haruna -y
+sudo apt install tmux neovim htop wget curl timeshift gdebi neofetch nala git flatpak haruna net-tools -y
 
 # Install build essential
 sudo nala install build-essential dkms linux-headers-$(uname -r) -y
