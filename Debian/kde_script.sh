8fd2c84c835690ba16a015dff6aaa9b428175624
Remus Sferle <119886030+SarCoptU@users.noreply.github.com>
2024-05-05 09:34:13 +0100

    Create kde_script.sh

---
+++ Debian/kde_script.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Remove some default home folders
+rm -rf ~/Videos ~/Templates ~/Music ~/Public
+
+# Update repositories and install main apps
+sudo apt update
+sudo apt upgrade
+
+sudo apt install tmux neovim htop wget curl timeshift neofetch nala git flatpak haruna net-tools -y
+
+# Install build essential
+sudo apt install build-essential dkms linux-headers-$(uname -r) -y
+
+# Add microsoft fonts
+sudo apt-add-repository contrib non-free -y
+sudo apt install software-properties-common ttf-mscorefonts-installer -y
+
+# Install restricted extras - non open source codecs 
+sudo apt install libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi -y
+
+# Neovim configuration: add line numbers and check syntax
+mkdir ~/.config/nvim
+echo "set number" >> ~/.config/nvim/init.vim
+echo "syntax on" >> ~/.config/nvim/init.vim
+
+# Install and configure firewall
+sudo apt install ufw -y
+sudo ufw enable
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+# Using the above commands, all the incoming traffic except ssh is blocked. All outgoing traffic allowed.
+
+# discord install
+wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
+sudo gdebi discord.deb
+# install taskbar notification ?source
+
+# fingerprint authentication https://wiki.debian.org/SecurityManagement/fingerprint%20authentication
+sudo apt install fprintd libpam-fprintd
+# then follow the instruction at Settings/User/Fingerprint Login
+
+# install oh my zsh
+# fist install zsh
+sudo apt install -y zsh
+# change default shell to zsh then restart
+chsh -s $(which zsh)
+# download and install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# for themes that require powerline fonts
+sudo apt install -y fonts-powerline
+# restart terminal and apply changes
+source ~/.zshrc
+
+# install kali-like theme and 
+wget -O ~/.oh-my-zsh/themes/kali-like.zsh-theme https://raw.githubusercontent.com/clamy54/kali-like-zsh-theme/master/kali-like.zsh-theme
+# nvim ~/.zshrc
+# Set ZSH_THEME="current_theme" to ZSH_THEME="kali-like" 
+# restart terminal and apply changes
+source ~/.zshrc
