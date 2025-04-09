0a9084c71bdc45cc3c42d0bab2feafa99a904eb3
Remus Sferle <119886030+SarCoptU@users.noreply.github.com>
2025-04-09 02:16:35 +0100

    Create kde_debian_12.sh

---
+++ kde_debian_12.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Debian 12 kde with standard utilities
+
+# Remove some default home folders
+rm -rf ~/Videos ~/Templates ~/Music ~/Public
+
+# Update repositories and install main apps
+sudo apt update
+sudo apt upgrade
+
+sudo apt install tmux neovim htop wget curl timeshift neofetch nala git flatpak haruna net-tools -y
+# add blueman if xfce
+
+# Install build essential
+sudo nala install build-essential dkms linux-headers-$(uname -r) -y
+
+# Add microsoft fonts
+sudo apt-add-repository contrib non-free -y
+sudo nala install software-properties-common ttf-mscorefonts-installer -y
+
+# Install restricted extras - non open source codecs 
+sudo nala install libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi -y
+
+# Add flatpak backup repo
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo apt install plasma-discover-backend-flatpak # for kde
+
+# Neovim configuration: add line numbers and check syntax
+mkdir ~/.config/nvim
+echo "set number" >> ~/.config/nvim/init.vim
+echo "syntax on" >> ~/.config/nvim/init.vim
+
+# Install and configure firewall
+sudo nala install ufw -y
+sudo ufw enable
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+# Using the above commands, all the incoming traffic except ssh is blocked. All outgoing traffic allowed.
+
+# discord, spotify install
+flatpak install discord
+flatpak install spotify
+
+# swap left control with caps lock on US layout
+setxkbmap -layout us -option ctrl:swapcaps
+
+# install oh my zsh
+# first install zsh
+sudo nala install -y zsh
+# change default shell to zsh then restart
+chsh -s $(which zsh)
+# download and install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# for themes that require powerline fonts
+sudo nala install -y fonts-powerline
+# restart terminal and apply changes
+source ~/.zshrc
+
+# install kali-like theme and 
+wget -O ~/.oh-my-zsh/themes/kali-like.zsh-theme https://raw.githubusercontent.com/clamy54/kali-like-zsh-theme/master/kali-like.zsh-theme
+# nvim ~/.zshrc
+# Set ZSH_THEME="current_theme" to ZSH_THEME="kali-like" 
+# restart terminal and apply changes
+source ~/.zshrc
+
+# Install steam https://wiki.debian.org/Steam
+sudo dpkg --add-architecture i386
+sudo nala update -y
+sudo nala install steam-installer
+sudo nala install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
