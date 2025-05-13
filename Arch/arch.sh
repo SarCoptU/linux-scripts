95cf0a0527bed4ca2d8028e02993de33e904afa4
Remus Sferle <119886030+SarCoptU@users.noreply.github.com>
2025-05-14 00:31:43 +0100

    Update arch.sh

--- Arch/arch.sh
+++ Arch/arch.sh
@@ -4,4 +4,11 @@
 sudo pacman -Syu
 
 # Install software
-sudo pacman -S git kitty zsh haruna waybar hyprland tmux nvim
+sudo pacman -S git curl wget kitty zsh haruna waybar hyprland tmux nvim
+
+# Change default shell to zsh then restart
+chsh -s $(which zsh)
+
+# Download and install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
