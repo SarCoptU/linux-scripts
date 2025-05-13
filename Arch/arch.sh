5cce6092b670f545c14b6061ea839a282d540e0f
Remus Sferle <119886030+SarCoptU@users.noreply.github.com>
2025-05-14 00:39:53 +0100

    Update arch.sh

--- Arch/arch.sh
+++ Arch/arch.sh
@@ -6,9 +6,21 @@ sudo pacman -Syu
 # Install software
 sudo pacman -S git curl wget kitty zsh haruna waybar hyprland tmux nvim
 
-# Change default shell to zsh then restart
+# Install fonts
+sudo pacman -S fonts-powerline
+
+# Change default shell to zsh then restart/logout
 chsh -s $(which zsh)
 
 # Download and install oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ 
+# Restart terminal and apply changes
+source ~/.zshrc
 
+# install kali-like theme and 
+wget -O ~/.oh-my-zsh/themes/kali-like.zsh-theme https://raw.githubusercontent.com/clamy54/kali-like-zsh-theme/master/kali-like.zsh-theme
+# nvim ~/.zshrc
+# Set ZSH_THEME="current_theme" to ZSH_THEME="kali-like" 
+# Restart terminal and apply changes
+source ~/.zshrc
