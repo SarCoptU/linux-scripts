ae709777909d8272b0153c39c2ad474b0868ba52
Remus Sferle <119886030+SarCoptU@users.noreply.github.com>
2025-06-04 23:00:23 +0100

    Update arch.sh

--- Arch/arch.sh
+++ Arch/arch.sh
@@ -1,1 +1,28 @@
-#! /bin/bash
+#!/bin/bash
+
+# xfce install
+
+# remove unessential home directories
+sudo rm -rf ~/Desktop ~/Templates ~/Videos 
+
+# install xfce
+sudo -Syu xfce xfce-goodies 
+
+# install essential programs
+sudo -S curl wget git gh zsh tmux nvim haruna stow hyprland hyprland-utils alacritty waybar wofi
+
+# DM which works with xfce
+sudo pacman -S lightdm lightdm-gtk-greeter
+
+# fix for hybrid Nvidia/AMD gpus 
+# it seems to work only with xfce for now
+# but it might be ok for installing hyprland
+sudo pacman -S xf86-video-amdgpu vulkan-radeon mesa
+
+sudo pacman -S nvidia nvidia-utils nvidia-prime
+
+# enable LightDM 
+sudo systemctl enable LightDM.service
+
+sudo reboot
+
