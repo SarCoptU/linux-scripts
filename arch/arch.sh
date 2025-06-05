c31800acd9f097767bdd8119047b942a69b3a5de
SarCoptU <remussferle@gmail.com>
2025-06-05 14:04:22 +0100

    separate hyprland line, discord and flatpak added

--- arch/arch.sh
+++ arch/arch.sh
@@ -4,23 +4,21 @@
 sudo pacman -Syu xfce4 xfce4-goodies 
 
 # install essential programs
-sudo pacman -S firefox curl wget git github-cli zsh tmux nvim haruna tree stow hyprland alacritty waybar wofi
+sudo pacman -S firefox discord curl wget git github-cli zsh tmux nvim haruna tree stow
 
-# Thunar functionality - gnome virtual file system
-sudo pacman -S gvfs 
+# Thunar functionality - gnome virtual file system + flatpak
+sudo pacman -S gvfs flatpak
 
 # DM which works with xfce
 sudo pacman -S lightdm lightdm-gtk-greeter
 
 # fix for hybrid Nvidia/AMD gpus 
 # it seems to work only with xfce for now
-# but it might be ok for installing hyprland
 sudo pacman -S xf86-video-amdgpu vulkan-radeon mesa
-
 sudo pacman -S nvidia nvidia-utils nvidia-prime
 
 # font for hyprland
-sudo pacman -S ttf-font-awesome powerline powerline-fonts
+sudo pacman -S hyprland alacritty waybar wofi ttf-font-awesome powerline powerline-fonts
 
 # enable LightDM 
 sudo systemctl enable lightdm.service
