d495b5be61f29087c2d7b0bdcbdec683ad5fc7bf
SarCoptU <remussferle@gmail.com>
2025-06-06 09:00:58 +0100

    zsh/oh my zsh in extras file

--- arch/arch.sh
+++ arch/arch.sh
@@ -26,20 +26,3 @@ sudo systemctl enable lightdm.service
 
 # spotify/obsidian flatpak
 flatpak install obsidian spotify
-
-# Change default shell to zsh then restart/logout
-chsh -s $(which zsh)
-
-# Download and install oh my zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Restart terminal and apply changes
-source ~/.zshrc
-
-# install kali-like theme and 
-wget -O ~/.oh-my-zsh/themes/kali-like.zsh-theme https://raw.githubusercontent.com/clamy54/kali-like-zsh-theme/master/kali-like.zsh-theme
-# nvim ~/.zshrc
-# Set ZSH_THEME="current_theme" to ZSH_THEME="kali-like" 
-
-# Restart terminal and apply changes
-source ~/.zshrc
---
+++ arch/extras.md
@@ -0,0 +1,20 @@
+# zhs/oh my zsh install
+
+## Change default shell to zsh then restart/logout
+`chsh -s $(which zsh)`
+
+## Download and install oh my zsh
+`sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`
+
+## Restart terminal and apply changes
+`source ~/.zshrc`
+
+## install kali-like theme
+`wget -O ~/.oh-my-zsh/themes/kali-like.zsh-theme https://raw.githubusercontent.com/clamy54/kali-like-zsh-theme/master/kali-like.zsh-theme`
+
+`nvim ~/.zshrc`
+
+## Set ZSH_THEME="current_theme" to ZSH_THEME="kali-like" 
+
+## Restart terminal to apply changes
+`source ~/.zshrc`
